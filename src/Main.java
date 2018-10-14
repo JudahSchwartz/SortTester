860c2df61178dde6c04bac6e0fc9172f4f129dc9
Judah Schwartz <judahsch@gmail.com>
2018-10-14 17:59:51 -0400

    added insertion sort

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class Main {
-    static final int sizeOfArray = 10_000;
+    static final int sizeOfArray = 1_00_000;
     public static void main(String[] args) {
         //bubble sort
         var startTime = System.nanoTime();
@@ -22,81 +22,50 @@ public class Main {
         nums = ms.nums;
         System.out.println(String.format("Num comparisons for %s: %s, num copies %s, total time in nanoseconds %s","Merge Sort",nums.comparisons,nums.copyOps,totalTime));
 
+        startTime = System.nanoTime();
+        QuickSort qs = new QuickSort();
+        qs.sort(getUnsortedArray(),0,sizeOfArray-1);
+        totalTime = System.nanoTime() - startTime;
+        nums = qs.nums;
+        System.out.println(String.format("Num comparisons for %s: %s, num swaps %s, total time in nanoseconds %s","Quick Sort",nums.comparisons,nums.swaps,totalTime));
+
+
+        startTime = System.nanoTime();
+        nums = insertionSort(getUnsortedArray());
+        totalTime = System.nanoTime() - startTime;
+        System.out.println(String.format("Num copyOperations for %s: %s, num comparisons %s, total time in nanoseconds %s","Selection Sort",nums.copyOps,nums.comparisons,totalTime));
+
     }
+
     // Merges two subarrays of arr[].
     // First subarray is arr[l..m]
     // Second subarray is arr[m+1..r]
-    void merge(int arr[], int l, int m, int r)
+
+
+    static EfficiencyNumbers insertionSort(int arr[])
     {
-        // Find sizes of two subarrays to be merged
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        /* Create temp arrays */
-        int L[] = new int [n1];
-        int R[] = new int [n2];
-
-        /*Copy data to temp arrays*/
-        for (int i=0; i<n1; ++i)
-            L[i] = arr[l + i];
-        for (int j=0; j<n2; ++j)
-            R[j] = arr[m + 1+ j];
-
-
-        /* Merge the temp arrays */
-
-        // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
-
-        // Initial index of merged subarry array
-        int k = l;
-        while (i < n1 && j < n2)
+        var nums = new EfficiencyNumbers();
+        int n = arr.length;
+        for (int i=1; i<n; ++i)
         {
-            if (L[i] <= R[j])
+            nums.comparisons++;
+            int key = arr[i];
+            int j = i-1;
+
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j>=0 && arr[j] > key)
             {
-                arr[k] = L[i];
-                i++;
+                nums.comparisons+=2;
+                arr[j+1] = arr[j];
+                j = j-1;
+                nums.copyOps+=2;
             }
-            else
-            {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
+            arr[j+1] = key;
         }
-
-        /* Copy remaining elements of L[] if any */
-        while (i < n1)
-        {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        /* Copy remaining elements of R[] if any */
-        while (j < n2)
-        {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
+        return nums;
     }
-    void sort(int arr[], int l, int r)
-    {
-        if (l < r)
-        {
-            // Find the middle point
-            int m = (l+r)/2;
-
-            // Sort first and second halves
-            sort(arr, l, m);
-            sort(arr , m+1, r);
-
-            // Merge the sorted halves
-            merge(arr, l, m, r);
-        }
-    }
-
     static EfficiencyNumbers selectionSort(int arr[]) {
         var nums = new EfficiencyNumbers();
         int n = arr.length;
@@ -235,6 +204,65 @@ class MergeSort {
 
             // Merge the sorted halves
             merge(arr, l, m, r);
+        }
+    }
+}
+
+// Java program for implementation of QuickSort
+class QuickSort {
+
+    public EfficiencyNumbers nums = new EfficiencyNumbers();
+
+    /* This function takes last element as pivot,
+           places the pivot element at its correct
+           position in sorted array, and places all
+           smaller (smaller than pivot) to left of
+           pivot and all greater elements to right
+           of pivot */
+    int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1); // index of smaller element
+        for (int j = low; j < high; j++) {
+            nums.comparisons++;
+            // If current element is smaller than or
+            // equal to pivot
+            nums.comparisons++;
+            if (arr[j] <= pivot) {
+                i++;
+
+                // swap arr[i] and arr[j]
+                nums.swaps++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        nums.swaps++;
+
+        return i + 1;
+    }
+
+
+    /* The main function that implements QuickSort()
+      arr[] --> Array to be sorted,
+      low  --> Starting index,
+      high  --> Ending index */
+    void sort(int arr[], int low, int high) {
+        if (low < high) {
+            nums.comparisons++;
+            /* pi is partitioning index, arr[pi] is
+              now at right place */
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before
+            // partition and after partition
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
         }
     }
 }
